7da522e5304aeda6fa47a616e82ad13e3639d77d
Vineet-kumar8757 <vineetkumar291020@gmail.com>
2022-11-07 21:34:18 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import AddProduct from "./Components/AddProduct";
+import Cart from "./Components/Cart";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import Navbar from "./Components/Navbar";
+import OrderedProducts from "./Components/OrderedProducts";
+import Product from "./Components/Product";
+import Products from "./Components/Products";
+import Profile from "./Components/Profile";
+import SignUp2 from "./Components/SignUp2";
+import EditProfile from "./Components/EditProfile"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={Products} />
+        <Route exact path="/products/getById/:productId" component={Product}/>
+        <Route exact path="/signup" component={SignUp2}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/cart" component={Cart}/>
+        <Route exact path="/profile" component={Profile}/>
+        <Route exact path="/orderedProducts" component={OrderedProducts}/>
+        <Route exact path="/addProduct" component={AddProduct}/>
+        <Route exact path="/editProfile" component={EditProfile}/>
+      </Switch>
+    </>
   );
 }
 
